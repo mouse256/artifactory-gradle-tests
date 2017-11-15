7a26852f09016ccb73ca77a8f283ddba3ecb6716
Tom Billiet <tom.billiet@technicolor.com>
2017-11-15 09:27:39 +0100

    Update

---
+++ test2/module3/main/java/org/example/module3/Module3.java
@@ -0,0 +1,15 @@
+package org.example.module3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Module3 {
+    private static final Logger LOG = LoggerFactory.getLogger(Module3.class);
+
+
+    public static void main(String[] args) {
+        LOG.info("Hello world");
+    }
+
+}
+
